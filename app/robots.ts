284b347497b4639f2afe5a4e2bfb14a281df1765
seydiop07 <seydiop07@barakashop.local>
2026-06-16 00:25:47 +0000

    feat: add robots.txt configuration to restrict access to sensitive routes

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,21 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: '/private/',
-        },
+        rules: [
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: [
+                    '/admin/',
+                    '/admin',
+                    '/api/',
+                    '/login',
+                    '/maintenance',
+                    '/private/',
+                    '/_next/',
+                ],
+            },
+        ],
         sitemap: 'https://baraka.sn/sitemap.xml',
     }
 }
